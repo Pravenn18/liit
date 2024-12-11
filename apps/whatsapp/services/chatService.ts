7ecde784294db2c9118efc701727aeb6e96dfebb
Pravenn18 <insanepraveen2018@gmail.com>
2024-12-12 00:44:32 +0530

    feat(whatsapp): implement group chat creation and related functionalities

--- apps/whatsapp/services/chatService.ts
+++ apps/whatsapp/services/chatService.ts
@@ -1,9 +1,11 @@
 import { supabase } from '../utils/supabase';
 import { Chat } from '@/types/Chat';
 import { getUserByPhone } from './userService';
+import { User } from '@/types/User';
 
 export const getOrCreateChat = async (
   participants: string[],
+  group: boolean = false,
 ): Promise<string> => {
   // Check if a chat already exists between the participants
   const { data, error } = await supabase
@@ -25,7 +27,7 @@ export const getOrCreateChat = async (
     // Create a new chat
     const { data: newChat, error: createError } = await supabase
       .from('chats')
-      .insert([{ participants, is_group: false }])
+      .insert([{ participants, is_group: group }])
       .select()
       .single();
 
@@ -62,4 +64,59 @@ export const getReceiverId = async (
   }
 
   return null;
+};
+
+export const createGroupChat = async (
+  groupName: string,
+  contacts: string[],
+  admin: string,
+) => {
+  const adminId = await getUserByPhone(admin);
+  const contactIds = await Promise.all(
+    contacts.map(async (contact) => {
+      const contactData = await getUserByPhone(contact);
+      return contactData ? contactData.id : null;
+    }),
+  );
+  const validContactIds = contactIds.filter((id) => id !== null) as string[];
+  const createGroupChatData = await getOrCreateChat(validContactIds, true);
+  const { data, error } = await supabase
+    .from('groups')
+    .insert([
+      { name: groupName, chat_id: createGroupChatData, admin: adminId?.id },
+    ]);
+
+  if (error) {
+    console.error('Error creating group chat:', error);
+    throw error;
+  }
+  return data;
+};
+
+export const getGroupsByAdmin = async (adminId: string) => {
+  const { data, error } = await supabase
+    .from('groups')
+    .select('*')
+    .eq('admin', adminId);
+
+  if (error) {
+    console.error('Error fetching groups:', error);
+    throw error;
+  }
+
+  return data;
+};
+
+export const getChatIdData = async (chatId: string) => {
+  const { data, error } = await supabase
+    .from('chats')
+    .select('*')
+    .eq('id', chatId);
+
+  if (error) {
+    console.error('Error fetching groups:', error);
+    throw error;
+  }
+
+  return data;
 };
